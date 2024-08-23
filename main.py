d75931f8096621a5414a867ce08b3778e637a9d1
Kushagr <75080533+Kushagr-25@users.noreply.github.com>
2024-08-24 02:56:09 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from src.data_preprocessing import load_and_preprocess_data
 from src.build_rules import generate_rules
 from src.recommend import recommend_products
-
+import pandas as pd
 
 def main():
     file_path = 'data/PreprocessedData.xlsx'
+    image_file_path = 'data/unique_items_with_images.csv'
+    base_image_path = 'content/images'  # Set this to the directory containing your images
 
     print("Starting the recommendation system pipeline")
 
     # Load and preprocess data
     basket = load_and_preprocess_data(file_path)
 
+    # Load product images data
+    product_images_df = pd.read_csv(image_file_path)
+    product_images = dict(zip(product_images_df['Description'], product_images_df['Image_File']))
+
     # Generate association rules
     rules = generate_rules(basket)
 
     # Example usage: Get recommendations for a specific product
-    product_to_check = 'WHITE HANGING HEART T-LIGHT HOLDER'
-    recommendations = recommend_products(rules, product_to_check)
+    product_to_check = 'WOODEN FRAME ANTIQUE WHITE '
+    recommendations = recommend_products(rules, product_to_check, product_images, base_image_path)
 
     if recommendations is not None:
         print("Recommendation system pipeline completed")
